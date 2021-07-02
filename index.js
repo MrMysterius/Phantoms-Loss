8b7ea4954eac16c4a7f92b650c8abff4f4f583d5
MrMysterius <official.mrmysterius@gmail.com>
2021-07-02 16:14:45 +0200

    barebone index

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-require('dotenv').config();
+require("dotenv").config();
+
+const Discord = require("discord.js");
+const express = require("express");
+
+const bot = new Discord.Client();
+const app = new express();
+
+app.get("/", (req, res) => {});
+
+bot.login(process.env.DISCORD_TOKEN || process.exit(10));
+app.listen(process.env.PORT || 3000);
