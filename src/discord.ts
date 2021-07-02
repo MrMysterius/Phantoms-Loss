1e4a66e675f7e2b21df5fdf8b570c473d2366bcf
MrMysterius <official.mrmysterius@gmail.com>
2021-07-02 20:57:25 +0200

    args and command parsing

--- src/discord.ts
+++ src/discord.ts
@@ -2,4 +2,7 @@ import * as Discord from "discord.js";
 
 export async function onMessage(message: Discord.Message) {
   if (!message.content.startsWith(process.env.PREFIX || "#") || message.author.bot) return;
+  const args = message.content.slice(0, (process.env.PREFIX || "#").length).split(" ");
+  const command = args.shift();
+
 }
