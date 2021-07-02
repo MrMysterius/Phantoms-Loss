c6cf50c42e008624bca2e4e485e5603f907cbd05
MrMysterius <official.mrmysterius@gmail.com>
2021-07-03 00:02:50 +0200

    added addcode

--- src/commands/addcode.ts
+++ src/commands/addcode.ts
@@ -1,8 +1,9 @@
 import * as Discord from "discord.js";
 
-import { createFailedEmbed } from "../discord";
+import { createFailedEmbed, createLoadingEmbed, createSuccessEmbed } from "../discord";
+import { dbAddCode, dbCodeSetMessageID, userData } from "../database";
 
-import { userData } from "../database";
+import { bot } from "..";
 
 export async function addCode(message: Discord.Message, args: Array<string>, user: userData) {
   if (!args[0] || args[0].length != 172) {
@@ -15,4 +16,36 @@ export async function addCode(message: Discord.Message, args: Array<string>, use
       .catch();
     return;
   }
+
+  const dm = await message.author.createDM();
+  dm.send(await createLoadingEmbed(message, " Adding Code"))
+    .then(async (msg) => {
+      const code_id = await dbAddCode(message.author.id, args[0]);
+      if (!code_id) {
+        msg.edit(createFailedEmbed(message, "Something Went Wrong"));
+      }
+      msg.edit(createSuccessEmbed(message, "Added Code"));
+      if (msg.deletable) msg.delete({ timeout: 120000 });
+      dm.send(createLoadingEmbed(message, "Add more information? (Please Wait Until Loaded)"))
+        .then(async (msg) => {
+          await dbCodeSetMessageID(code_id as string, msg.id);
+          await msg.react(process.env.E_UNCOMMON as string);
+          await msg.react(process.env.E_RARE as string);
+          await msg.react(process.env.E_EPIC as string);
+          await msg.react(process.env.E_LEGENDARY as string);
+          await msg.react(process.env.E_GUARDIAN_MASK as string);
+          await msg.react(process.env.E_GUARDIAN_DEVOUR as string);
+          await msg.react(process.env.E_GUARDIAN_EYE as string);
+          await msg.react(process.env.E_SUCCESS as string);
+          msg
+            .edit(
+              createSuccessEmbed(message, `Add more Information, then click on ${process.env.E_SUCCESS}`).setDescription(
+                "Click on the Glyphs to add one to the count. Click on the Guardian to select which guardian was in the temple."
+              )
+            )
+            .catch();
+        })
+        .catch();
+    })
+    .catch();
 }
