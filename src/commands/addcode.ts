59eca486617c6ab6d3a68a15475fdb551a03f389
MrMysterius <official.mrmysterius@gmail.com>
2021-07-02 22:46:26 +0200

    addcode missing argument

--- src/commands/addcode.ts
+++ src/commands/addcode.ts
@@ -1,3 +1,16 @@
 import * as Discord from "discord.js";
 
-export async function addCode(message: Discord.Message, args: Array<string>) {}
+import { createFailedEmbed } from "../discord";
+
+export async function addCode(message: Discord.Message, args: Array<string>) {
+  if (!args[0] || args[0].length != 172) {
+    const dm = await message.author.createDM();
+    const embed = createFailedEmbed(message, "Code Missing");
+    dm.send(embed)
+      .then((msg) => {
+        if (msg.deletable) msg.delete({ timeout: 120000 });
+      })
+      .catch();
+    return;
+  }
+}
