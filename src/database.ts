5e0e1449ad6f530f82fdad4d7b86d17e1a052627
MrMysterius <official.mrmysterius@gmail.com>
2021-07-02 19:13:12 +0200

    added oath functions and steam connections getter

--- src/database.ts
+++ src/database.ts
@@ -25,4 +25,26 @@ export interface userObject {
   public_flags?: number;
 }
 
-export function dbAddOAuth2(oAuth2Data: OAuth2Data, userData: userObject) {}
+export interface connectionsData {
+  id: string;
+  name: string;
+  type: string;
+  revoked?: boolean;
+  integrations?: Array<{ [key: string]: any }>;
+  verified: boolean;
+  friend_sync: boolean;
+  show_activity: boolean;
+  visibility: number;
+}
+
+export function dbAddOAuth2(oAuth2Data: OAuth2Data, userData: userObject, steamConnections: Array<connectionsData>) {
+  try {
+    db.prepare(
+      `INSERT INTO users (user_id, username, steam_username, access_token, refresh_token, scope, token_type, level, xp) VALUES ("${userData.id}", "${
+        userData.username
+      }", "${steamConnections.reduce((p, c) => (p += c + ";"), "")}", "${oAuth2Data.access_token}", "${oAuth2Data.refresh_token}", "${oAuth2Data.scope}", "${
+        oAuth2Data.token_type
+      }", 1, 0)`
+    ).run();
+  } catch (err) {}
+}
--- src/oauth2.ts
+++ src/oauth2.ts
@@ -1,4 +1,4 @@
-import { OAuth2Data, dbAddOAuth2, userObject } from "./database";
+import { OAuth2Data, connectionsData, dbAddOAuth2, userObject } from "./database";
 
 import { URLSearchParams } from "url";
 import { default as fetch } from "node-fetch";
@@ -24,15 +24,37 @@ export async function newOAuth2(code: string) {
 
   if (!oauth2Data.access_token) return;
 
+  const meData = await getUserInfo(oauth2Data.token_type, oauth2Data.access_token);
+
+  if (!meData.id) return;
+
+  const filteredConnections = await getSteamConnections(oauth2Data.token_type, oauth2Data.access_token);
+
+  dbAddOAuth2(oauth2Data, meData, filteredConnections);
+}
+
+export async function getUserInfo(token_type: string, access_token: string) {
   const resMe = await fetch("https://discord.com/api/users/@me", {
     headers: {
-      authorization: `${oauth2Data.token_type} ${oauth2Data.access_token}`,
+      authorization: `${token_type} ${access_token}`,
     },
   });
 
   const meData: userObject = await resMe.json();
 
-  if (!meData.id) return;
+  return meData;
+}
 
-  dbAddOAuth2(oauth2Data, meData);
+export async function getSteamConnections(token_type: string, access_token: string) {
+  const resConnections = await fetch("https://discord.com/api/users/@me/connections", {
+    headers: {
+      authorization: `${token_type} ${access_token}`,
+    },
+  });
+
+  const connectionsData: Array<connectionsData> = await resConnections.json();
+
+  const filteredConnections = connectionsData.filter((data) => data.type == "steam");
+
+  return filteredConnections;
 }
