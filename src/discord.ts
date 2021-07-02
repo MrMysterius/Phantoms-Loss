4677062cff45dcb5661ead44767e3e5f8042e77d
MrMysterius <official.mrmysterius@gmail.com>
2021-07-02 21:57:14 +0200

    spellcheck stuff

--- src/discord.ts
+++ src/discord.ts
@@ -47,7 +47,7 @@ export async function help(message: Discord.Message, args: Array<String>) {
 
   embed.addField(
     "Disclaimer and Dataprivacy Notice",
-    "You need to setup an account for almost everything on this bot. This also stores data about your user account (this includes Discord), connections on your Discord account and sharecodes that give the bot. As well as timestamps of certain actions. I also ban unfair or cheating players. For this premise I use a verification System that display your Steam Username somebody to someone random. If you don't want to get false detected keep your steam connection on your Discord account up to date.\n\nThe bot also primarly is used in dm's so please keep them open."
+    "You need to setup an account for almost everything on this bot. This also stores data about your user account (this includes Discord), connections on your Discord account and sharecodes that you give the bot. As well as timestamps of certain actions. I also ban unfair or cheating players. For this premise I use a verification System that display your Steam Username to somebody random.\nIf you don't want to get false detected keep your steam connection on your Discord account up to date.\n\nThe bot also primarly is used in dm's so please keep them open to the Bot."
   );
 
   embed.addField(
