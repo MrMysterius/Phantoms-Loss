cfef2cae73a90a2d857638c5507b32e48a56e278
MrMysterius <official.mrmysterius@gmail.com>
2021-07-02 17:53:03 +0200

    pathed db location

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as Discord from "discord.js";
 import * as dotenv from "dotenv";
+import * as path from "path";
 
 import { default as bsql } from "better-sqlite3";
 import { default as express } from "express";
@@ -7,7 +8,7 @@ import { expressMain } from "./express";
 
 dotenv.config();
 
-export const db = new bsql("phantom_loss.db");
+export const db = new bsql(path.join(__dirname, "phantom_loss.db"));
 const bot = new Discord.Client();
 const app = express();
 
