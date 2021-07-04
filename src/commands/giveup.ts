66b8d9cd9a5c069b90c65e00417feb194fbd7ef5
MrMysterius <official.mrmysterius@gmail.com>
2021-07-04 14:48:55 +0200

    added missing argument

--- src/commands/giveup.ts
+++ src/commands/giveup.ts
@@ -1,14 +1,14 @@
 import * as Discord from "discord.js";
 
+import { codeStatus, dbCodeAssignsGet, dbCodeUnassign, userData } from "../database";
 import { createFailedEmbed, createLoadingEmbed, createSuccessEmbed } from "../discord";
-import { dbCodeAssignsGet, dbCodeUnassign, userData } from "../database";
 
 export async function giveUp(message: Discord.Message, args: Array<string>, user: userData) {
   const dm = await message.author.createDM();
   const msg = await dm.send(await createLoadingEmbed(message, "Giving Up...")).catch();
   if (!msg) return;
 
-  const assignedCodes = await dbCodeAssignsGet(message.author.id);
+  const assignedCodes = await dbCodeAssignsGet(message.author.id, codeStatus.assigned);
 
   if (assignedCodes.length == 0) {
     await msg.edit(await createFailedEmbed(message, "No code assgined at the moment"));
