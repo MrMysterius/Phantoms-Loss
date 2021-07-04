1d91f6aef73df0a387f1a99e02b01075f5dbf8ce
MrMysterius <official.mrmysterius@gmail.com>
2021-07-04 16:10:50 +0200

    fixed get code

--- src/commands/getcode.ts
+++ src/commands/getcode.ts
@@ -1,6 +1,6 @@
 import * as Discord from "discord.js";
 
-import { codeStatus, dbCodeAssign, dbCodeAssignsGet, dbCodesGetOpen, userData } from "../database";
+import { codeData, codeStatus, dbCodeAssign, dbCodeAssignsGet, dbCodesGetOpen, userData } from "../database";
 import { createFailedEmbed, createLoadingEmbed, createSuccessEmbed, dmRestricted } from "../discord";
 
 export async function getCode(message: Discord.Message, args: Array<string>, user: userData) {
@@ -12,13 +12,13 @@ export async function getCode(message: Discord.Message, args: Array<string>, use
 
   if (assignedCodes.length > 0) {
     const embed = await createSuccessEmbed(message, "Your current code:");
-    embed.addField(`Code - ${assignedCodes[0].code}`, "```" + assignedCodes[0].code + "```");
+    embed.addField(`Code - ${assignedCodes[0].code_id}`, "```" + assignedCodes[0].code + "```");
     embed.addField("Attempts", assignedCodes[0].attempts);
     embed.addField(
       "Keys",
       `${process.env.E_UNCOMMON}: ${assignedCodes[0].uncommon}\n${process.env.E_RARE}: ${assignedCodes[0].rare}\n${process.env.E_EPIC}: ${assignedCodes[0].epic}\n${process.env.E_LEGENDARY}: ${assignedCodes[0].legendary}`
     );
-    let guardian = "";
+    let guardian = "-";
     switch (assignedCodes[0].guardian) {
       case "MASK":
         guardian = process.env.E_GUARDIAN_MASK as string;
@@ -37,12 +37,13 @@ export async function getCode(message: Discord.Message, args: Array<string>, use
 
   const openCodes = await dbCodesGetOpen(user.user_id);
 
-  if (openCodes.length == 0) return await createFailedEmbed(message, "No codes available :/");
+  if (openCodes.length == 0) return await msg.edit(createFailedEmbed(message, "No codes available :/"));
 
-  const averageAttempts = openCodes.reduce((p, c) => (p += c.attempts), 0) / open.length;
+  const averageAttempts = openCodes.reduce((p, c) => (p += c.attempts), 0) / openCodes.length;
 
-  let code = openCodes[0];
-  code.attempts - 1;
+  let code: codeData;
+  code = { ...openCodes[0] };
+  code.attempts = -1;
 
   while (code.attempts < averageAttempts) {
     code = openCodes[Math.floor(Math.random() * openCodes.length)];
@@ -51,13 +52,13 @@ export async function getCode(message: Discord.Message, args: Array<string>, use
   await dbCodeAssign(user.user_id, code.code_id);
 
   const embed = await createSuccessEmbed(message, "Your current code:");
-  embed.addField(`Code - ${code.code}`, "```" + code.code + "```");
+  embed.addField(`Code - ${code.code_id}`, "```" + code.code + "```");
   embed.addField("Attempts", code.attempts);
   embed.addField(
     "Keys",
     `${process.env.E_UNCOMMON}: ${code.uncommon}\n${process.env.E_RARE}: ${code.rare}\n${process.env.E_EPIC}: ${code.epic}\n${process.env.E_LEGENDARY}: ${code.legendary}`
   );
-  let guardian = "";
+  let guardian = "-";
   switch (code.guardian) {
     case "MASK":
       guardian = process.env.E_GUARDIAN_MASK as string;
